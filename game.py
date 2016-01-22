c80842c4c7411ff70478e8168729563d813a2012
Elif Celik <elifyildizcelik@gmail.com>
2016-01-22 22:25:35 +0100

    speelboard

--- game.py
+++ game.py
@@ -2,19 +2,14 @@ import pygame, sys
 
 pygame.init()
 
-windowSize = (800, 600)
+boardImage = pygame.image.load("./images/Board.png")
 
+windowSize = boardImage.get_size()
 screen = pygame.display.set_mode(windowSize)
 
-myriadProFont = pygame.font.SysFont("Myriad Pro", 48)
-
-helloWorld = myriadProFont.render("Buy a ride", 1, (255, 0, 255), (255, 255, 255))
-
-helloWorldize = helloWorld.get_size()
+boardImageSize = boardImage.get_size()
 
 x, y = 0, 0
-directionX = 5
-directionY = 5
 
 clock = pygame.time.Clock()
 
@@ -26,17 +21,8 @@ while 1:
         if event.type == pygame.QUIT:
             sys.exit()
 
-    if x + helloWorldize[0] > windowSize[0] or x < 0:
-        directionX *= -1
-
-    if y + helloWorldize[1] > windowSize[1] or y < 0:
-        directionY *= -1
-
     screen.fill((0, 0, 0))
 
-    screen.blit(helloWorld, (x, y))
-
-    x += directionX
-    y += directionY
+    screen.blit(boardImage, (0, 0))
 
     pygame.display.update()
